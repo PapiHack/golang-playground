3ad1b654553f4d22ced92aca16098d103b242387
PapiHack <mssmbaye@gmail.com>
2023-02-09 19:14:39 +0000

    Discovering slices

---
+++ 08slices/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Welcome to our class on slices")
+
+	var fruitList = []string{"Apple", "Tomato", "Peach"}
+	fmt.Printf("Type of fruit list is %T\n", fruitList)
+
+	fruitList = append(fruitList, "Mango", "Banana")
+	fmt.Println(fruitList)
+
+	fruitList = append(fruitList[:3])
+	fmt.Println(fruitList)
+
+	highScores := make([]int, 4)
+
+	highScores[0] = 123
+	highScores[1] = 321
+	highScores[2] = 243
+	highScores[3] = 432
+	// highScores[4] = 532
+
+	highScores = append(highScores, 543, 666, 376)
+
+	fmt.Println(highScores)
+	fmt.Println(sort.IntsAreSorted(highScores))
+	sort.Ints(highScores)
+	fmt.Println(highScores)
+	fmt.Println(sort.IntsAreSorted(highScores))
+}
