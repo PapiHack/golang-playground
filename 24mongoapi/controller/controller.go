1690876a0e20a3ad2109dba53785e277d4de9ef1
PapiHack <mssmbaye@gmail.com>
2023-03-22 16:45:15 +0000

    mongoapi: get all movies

--- 24mongoapi/controller/controller.go
+++ 24mongoapi/controller/controller.go
@@ -61,3 +61,45 @@ func updateOneMovie(movieId string) {
 	}
 	fmt.Println("[*] Modified count:", result.ModifiedCount)
 }
+
+func deleteOneMovie(movieId string) {
+	id, err := primitive.ObjectIDFromHex(movieId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filter := bson.M{"_id": id}
+	result, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("[*] Deleted count:", result.DeletedCount)
+}
+
+func deleteAllMovie() {
+	result, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("[*] Number of movies deleted:", result.DeletedCount)
+}
+
+func getAllMovies() []primitive.M {
+	cursor, err := collection.Find(context.Background(), bson.D{{}})
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	var movies []primitive.M
+
+	for cursor.Next(context.Background()) {
+		var movie bson.M
+		er := cursor.Decode(&movie)
+		if er != nil {
+			log.Fatal(er)
+		}
+		movies = append(movies, movie)
+	}
+
+	return movies
+}
