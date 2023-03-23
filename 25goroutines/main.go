eb009efd64d9ddb307e2b5ded13e72d2718f89ac
PapiHack <mssmbaye@gmail.com>
2023-03-23 19:04:26 +0000

    Golang WaitGroups

--- 25goroutines/main.go
+++ 25goroutines/main.go
@@ -2,17 +2,46 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
+var wg sync.WaitGroup // Usually pointer
+
 func main() {
-	go greeter("Hello")
-	greeter("World")
+	// go greeter("Hello")
+	// greeter("World")
+	websiteList := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
+	}
+
+	for _, site := range websiteList {
+		go getStatusCode(site)
+		wg.Add(1)
+	}
+
+	wg.Wait()
 }
 
-func greeter(s string) {
-	for i := 0; i < 6; i++ {
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(s)
+// func greeter(s string) {
+// 	for i := 0; i < 6; i++ {
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+	response, err := http.Get(endpoint)
+
+	if err != nil {
+		fmt.Println("OOPS in endpoint !")
+		panic(err)
 	}
+
+	fmt.Printf("%d status code for %s !\n", response.StatusCode, endpoint)
 }
